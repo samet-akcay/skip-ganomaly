815a722558dc184c962d80f0efd9bcc848972d79
akczay <smt.akcay@gmail.com>
2018-07-31 08:28:17 +0100

    mnist to run v2

--- experiments/run_mnist.sh
+++ experiments/run_mnist.sh
@@ -5,6 +5,6 @@
 for i in {0..9}
 do
     echo "Running mnist_$i"
-    python train.py --dataset mnist --isize 32 --nc 1 --niter 15 --anomaly_class $i
+    python train.py --dataset mnist --isize 32 --nc 1 --niter 15 --anomaly_class $i --model ganomaly2
 done
 exit 0
