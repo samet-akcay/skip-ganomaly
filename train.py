8ec8e717b294610c56e65a9b242dfba683ba073b
Samet Akcay <smt.akcay@gmail.com>
2019-08-13 15:59:49 +0100

    Encapsulated

--- train.py
+++ train.py
@@ -1,51 +1,29 @@
-# """
-# TRAIN GANOMALY
+"""
+TRAIN SKIP/GANOMALY
 
-# . Example: Run the following command from the terminal.
-#     run train.py                             \
-#         --model ganomaly                        \
-#         --dataset UCSD_Anomaly_Dataset/UCSDped1 \
-#         --batchsize 32                          \
-#         --isize 256                         \
-#         --nz 512                                \
-#         --ngf 64                               \
-#         --ndf 64
-# """
-
-
-##
-# LIBRARIES
-from __future__ import print_function
-
-from options import Options
-from lib.data.dataloader import load_data
-# from lib.models.ganomaly import Ganomaly
-from lib.models import load_model
-import copy
-
-##
-# def main():
-""" Training
+. Example: Run the following command from the terminal.
+    run train.py                                    \
+        --model <skipganomaly, ganomaly>            \
+        --dataset cifar10                           \
+        --abnormal_class airplane                   \
+        --display                                   \
 """
 
 ##
-# ARGUMENTS
-opt = Options().parse()
+# LIBRARIES
+
+from options import Options
+from lib.data.dataloader import load_data
+from lib.models import load_model
 
 ##
-# LOAD DATA
-dataloader = load_data(opt)
+def main():
+    """ Training
+    """
+    opt = Options().parse()
+    data = load_data(opt)
+    model = load_model(opt, data)
+    model.train()
 
-# # NOTE: this bit should go under the train method in v3
-# # TODO: new bit
-# opt_h = copy.copy(opt)
-# opt_h.model = 'aae'
-# model_h = load_model(opt_h, dataloader)
-# model_h.train(epochs=5)
-##
-# LOAD MODEL
-model = load_model(opt, dataloader)
-# model.netg_h.load_state_dict(model_h.netg.state_dict())
-##
-# TRAIN MODEL
-model.train()
+if __name__ == '__main__':
+    main()
