59840bd96df42cdb3230619ce3023aa5078b9654
Samet Akcay <smt.akcay@gmail.com>
2019-08-14 10:17:52 +0100

    Added folder dataset

--- lib/data/dataloader.py
+++ lib/data/dataloader.py
@@ -5,11 +5,12 @@ LOAD DATA from file.
 # pylint: disable=C0301,E1101,W0622,C0103,R0902,R0915
 
 ##
+import os
 from torchvision import transforms
 from torch.utils.data import DataLoader
-from torchvision.datasets import CIFAR10
-from torchvision.datasets import MNIST
-from torchvision.datasets import ImageFolder
+from torchvision.datasets import MNIST, CIFAR10, ImageFolder
+from lib.data.datasets import get_cifar_anomaly_dataset
+from lib.data.datasets import get_mnist_anomaly_dataset
 
 class Data:
     """ Dataloader containing train and valid sets.
@@ -39,8 +40,6 @@ def load_data(opt):
 
     ## CIFAR
     if opt.dataset in ['cifar10']:
-        from lib.data.datasets import get_cifar_anomaly_dataset
-
         transform = transforms.Compose([transforms.Resize(opt.isize),
                                         transforms.ToTensor(),
                                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -51,8 +50,6 @@ def load_data(opt):
 
     ## MNIST
     elif opt.dataset in ['mnist']:
-        from lib.data.datasets import get_mnist_anomaly_dataset
-
         transform = transforms.Compose([transforms.Resize(opt.isize),
                                         transforms.ToTensor(),
                                         transforms.Normalize((0.1307,), (0.3081,))])
@@ -62,9 +59,18 @@ def load_data(opt):
         valid_ds = MNIST(root='./data', train=False, download=True, transform=transform)
         train_ds, valid_ds = get_mnist_anomaly_dataset(train_ds, valid_ds, int(opt.abnormal_class))
 
+    # FOLDER
+    else:
+        transform = transforms.Compose([transforms.Resize(opt.isize),
+                                        transforms.CenterCrop(opt.isize),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), ])
+
+        train_ds = ImageFolder(os.path.join(opt.dataroot, 'train'), transform)
+        valid_ds = ImageFolder(os.path.join(opt.dataroot, 'test'), transform)
+
     ## DATALOADER
     train_dl = DataLoader(dataset=train_ds, batch_size=opt.batchsize, shuffle=True, drop_last=True)
     valid_dl = DataLoader(dataset=valid_ds, batch_size=opt.batchsize, shuffle=False, drop_last=False)
 
     return Data(train_dl, valid_dl)
-
